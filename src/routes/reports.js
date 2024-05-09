9a5f46d8fc0deca9cc7bc9ca011612458f8f2e7f
talfumier <henri.talfumier@gmail.com>
2024-05-09 17:35:59 +0200

    stats routes moved back to reports

--- src/routes/reports.js
+++ src/routes/reports.js
@@ -12,11 +12,13 @@ import {validateIntegerId} from "../models/validation/joiUtilityFunctions.js";
 const router = express.Router();
 
 let Salon = null,
-  Report = null;
+  Report = null,
+  connection = null;
 function setModels(req, res, next) {
   const models = getModels();
   Salon = models.Salon;
   Report = models.Report;
+  connection = models.connection;
   next();
 }
 /*BASIC*/
@@ -137,6 +139,53 @@ router.delete(
       message: `Report with id:${id} successfully deleted.`,
       data: report,
     });
+  })
+);
+/*STATISTICS*/
+router.get(
+  "/stats/raw/salon",
+  [authHandler, authValid, setModels], //raw data provided i.a.w logged-in user authorized salon(s)
+  routeHandler(async (req, res) => {
+    const sql = `SELECT s.id,s.name_salon,s.city,s.code AS code_dept,d.nom AS dept,reg.nom AS region,DATE_FORMAT(s.date_open,'%Y-%m-%d') AS date_open,
+      rpt.period,ROUND(rpt.fte,2) AS fte,ROUND(rpt.turn_around,2) AS 'turn-around',ROUND(rpt.turn_around/rpt.fte,0) AS 'turn-around/fte'
+      FROM salons.salons s 
+      JOIN salons.reports rpt ON s.id=rpt.salon_id
+      JOIN salons.depts d ON s.code=d.code
+      JOIN salons.regions reg ON reg.code=d.codeRegion
+      WHERE s.id IN(${req.user.salons})`;
+    const data = await connection.query(sql);
+    res.send({status: "OK", data});
+  })
+);
+router.get(
+  "/stats/dept/all",
+  [authHandler, authValid, setModels],
+  routeHandler(async (req, res) => {
+    const sql = `SELECT  MAX(s.code) AS code_dept,MAX(d.nom) AS dept,MAX(reg.nom) AS region,COUNT(DISTINCT s.id) AS 'total-salons',
+      ROUND(AVG(rpt.fte),2) AS 'avg-fte',SUM(ROUND(rpt.turn_around,2)) AS 'sum turn-around',COUNT(rpt.period) AS 'total-monthly-reports',
+      ROUND(SUM(rpt.turn_around)/(AVG(rpt.fte)*COUNT(rpt.period)),0) AS 'avg-monthly-turn-around/fte'
+      FROM salons.salons s 
+      JOIN salons.reports rpt ON s.id=rpt.salon_id
+      JOIN salons.depts d ON s.code=d.code
+      JOIN salons.regions reg ON reg.code=d.codeRegion
+      GROUP BY s.code`;
+    const data = await connection.query(sql);
+    res.send({status: "OK", data});
+  })
+);
+router.get(
+  "/stats/region/all",
+  [authHandler, authValid, setModels],
+  routeHandler(async (req, res) => {
+    const sql = `SELECT  MAX(reg.nom) AS region,COUNT(DISTINCT s.id) AS 'total-salons',LEFT(rpt.period,4) AS year,
+      ROUND(AVG(rpt.fte),2) AS 'avg-fte',SUM(ROUND(rpt.turn_around,2)) AS 'sum turn-around',COUNT(rpt.period) AS 'total-monthly-reports'
+      FROM salons.salons s 
+      JOIN salons.reports rpt ON s.id=rpt.salon_id
+      JOIN salons.depts d ON s.code=d.code
+      JOIN salons.regions reg ON reg.code=d.codeRegion
+      GROUP BY reg.nom,LEFT(rpt.period,4)`;
+    const data = await connection.query(sql);
+    res.send({status: "OK", data});
   })
 );
 export default router;
--- src/routes/users/profile.js
+++ src/routes/users/profile.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt";
 import {authHandler} from "../../middleware/authHandler.js";
 import {routeHandler} from "../../middleware/routeHandler.js";
 import {getModels, validateUser} from "../../models/sqlServerModels.js";
-import {BadRequest} from "../../models/validation/errors.js";
+import {BadRequest, Unauthorized} from "../../models/validation/errors.js";
 import {bodyCleanUp} from "./register.js";
 import {environment} from "../../config/environment.js";
 
@@ -27,6 +27,10 @@ router.patch(
     req.body = bodyCleanUp(req.body);
     const error = validateUser(req.body, "patch").error;
     if (error) return res.send(new BadRequest(error.details[0].message));
+    if (req.body.role && req.body.role === "admin")
+      return res.send(
+        new Unauthorized("admin account cannot be modified through the API.")
+      );
     if (req.body.pwd)
       req.body.pwd = await bcrypt.hash(req.body.pwd, environment.salt_rounds);
     await user.update(req.body);
@@ -44,6 +48,10 @@ router.delete(
     const id = req.user.id;
     const user = await User.findByPk(id);
     if (!user) return res.send(new BadRequest(`User with id:${id} not found.`));
+    if (user.role === "admin")
+      return res.send(
+        new Unauthorized("admin account cannot be deleted through the API.")
+      );
     await user.destroy(); //any related record in table users_salons will be removed by CASCADE DELETE
     user.pwd = undefined; //does not return the password
     res.send({
